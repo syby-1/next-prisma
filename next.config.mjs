0c5a0b3ca32ea3ce5c197630dc1444c72ae4577e
syby1 <1sibinkeliyas123@gmail.com>
2024-06-13 15:29:20 +0530

    Added bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+import withBundleAnalyzer from '@next/bundle-analyzer'
 const nextConfig = {
     experimental: {
         nextScriptWorkers: true,
       },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
