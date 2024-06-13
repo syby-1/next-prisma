c40c69d11c4155efadcf10dee4a626ddf24cbdc8
sibin k <1sibinkeliyas123@gmail.com>
2024-06-14 01:16:22 +0530

    Added the setTimout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <Script id="googleTag" strategy="afterInteractive">{`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          setTimeout(function() {
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.defer=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-NSKKPP2');
-          `}</Script>
+          }, 1500);
+`}</Script>
       </head>
       <body>
         <Provider>{children}</Provider>
