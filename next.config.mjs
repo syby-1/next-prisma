effb5c0ea451a4bf92cfa03921d8b915bb365087
syby1 <1sibinkeliyas123@gmail.com>
2024-06-13 15:32:10 +0530

    removed bundle analyle

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-import withBundleAnalyzer from '@next/bundle-analyzer'
 const nextConfig = {
     experimental: {
         nextScriptWorkers: true,
       },
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
