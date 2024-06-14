2605422da473b82c17089de76d88fbb04009af6f
syby1 <1sibinkeliyas123@gmail.com>
2024-06-14 12:32:15 +0530

    removed the  google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,14 +19,6 @@ export default function RootLayout({
       <head></head>
       <body>
         <Provider>{children}</Provider>
-        <Script defer id="googleTag" strategy="afterInteractive">{`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-NSKKPP2')
-            
-    `}</Script>
       </body>
     </html>
   );
