f01fa7f841ae2ebd938f84ea3ea708692d6726e2
syby1 <1sibinkeliyas123@gmail.com>
2024-06-14 11:32:41 +0530

    Added the Head and timout to gtm tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
 import { Provider } from '@/context/themeProvider';
 import Script from 'next/script';
-
-const inter = Inter({ subsets: ['latin'] });
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,29 +16,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head></head>
+      <Head>
+        <link rel="stylesheet" href="" />
+      </Head>
       <body>
         <Provider>{children}</Provider>
-        <Script
-          id="gTag"
-          async
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            setTimeout(function() {
-                (function() {
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'GTM-NSKKPP2');
-                    
-                    // Additional event pushed after GTM initialization
-                    dataLayer.push({'event': 'afterLoad'});
-                })();
-            }, 1500);
-        `,
-          }}
-        ></Script>
+          <Script defer id="googleTag" strategy="lazyOnload">{`
+      setTimeout(function() {
+          (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.defer=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NSKKPP2');
+      }, 1500);
+  `}</Script>
       </body>
     </html>
   );
