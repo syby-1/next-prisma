294f7ae24bea85e78d1d63805734c96d182b5bbd
syby1 <1sibinkeliyas123@gmail.com>
2024-06-14 11:44:23 +0530

    Updatd the code into useEffect innerHtml

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,22 +15,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   useEffect(() => {
-    const timer = setTimeout(() => {
-      (function (w : any, d : any, s : any, l : any, i : any) {
-        w[l] = w[l] || [];
-        w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-        var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.defer = true;
-        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', 'GTM-NSKKPP2');
-    }, 1500);
+    const script = document.createElement('script');
+    script.defer = true;
+    script.id = 'googleTag';
+    script.innerHTML = `
+        setTimeout(function() {
+            (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.defer=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NSKKPP2');
+        }, 1500);
+    `;
+    document.head.appendChild(script);
 
-    // Cleanup function to clear the timeout if the component unmounts
-    return () => clearTimeout(timer);
-  }, []);
+    return () => {
+        // Clean up the script element if the component unmounts
+        document.head.removeChild(script);
+    };
+}, []);
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
