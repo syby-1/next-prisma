5105bad9e6c708514a72a8f9dfc9b2a15abd2139
syby1 <1sibinkeliyas123@gmail.com>
2024-06-14 10:49:21 +0530

    changed into google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "@/context/themeProvider";
 import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,16 +22,8 @@ export default function RootLayout({
       <head></head>
       <body>
         <Provider>{children}</Provider>
+        <GoogleTagManager gtmId="GTM-NSKKPP2" />
       </body>
-      <footer>
-        <Script id="googleTag" strategy="afterInteractive">{`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NSKKPP2');
-            `}</Script>
-      </footer>
     </html>
   );
 }
