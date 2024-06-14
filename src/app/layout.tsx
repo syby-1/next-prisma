1954094ede2915ed24b4ff880f391fe94648ac09
syby1 <1sibinkeliyas123@gmail.com>
2024-06-14 11:01:17 +0530

    updated into next Script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <head></head>
       <body>
         <Provider>{children}</Provider>
-        <Script async id="googleTag" strategy="lazyOnload">{`
+        <Script defer id="googleTag" strategy="lazyOnload">{`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
