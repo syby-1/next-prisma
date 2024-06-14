5acc72567db8b622e46b066aa1497fd81537ee29
syby1 <1sibinkeliyas123@gmail.com>
2024-06-14 12:27:13 +0530

    Updatd the career360  code for google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Provider } from '@/context/themeProvider';
 import dynamic from 'next/dynamic';
-
-const GoogleTag = dynamic(() => import('../components/script/gtm'));
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +19,14 @@ export default function RootLayout({
       <head></head>
       <body>
         <Provider>{children}</Provider>
-        <GoogleTag />
+        <Script defer id="googleTag" strategy="afterInteractive">{`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NSKKPP2')
+            
+    `}</Script>
       </body>
     </html>
   );
